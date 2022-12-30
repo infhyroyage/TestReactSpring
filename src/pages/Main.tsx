6f113f94a6192d03c51d2778dcf725ef406e3cb8
Takeru KIDO <keidy2463@gmail.com>
2022-12-30 16:59:04 +0900

    Add useTrail

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 export const Main: FC<{}> = () => {
-  return <h1>Main</h1>;
+  return (
+    <>
+      <h1>Main</h1>
+      <Link to="/TestReactSpring/useTrail">useTrail</Link>
+    </>
+  );
 };
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,11 +2,13 @@ import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Main } from "../pages/Main";
 import { NotFound } from "../pages/NotFound";
+import { UseTrail } from "../pages/UseTrail";
 
 export const Router: FC<{}> = () => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/TestReactSpring/" element={<Main />} />
+      <Route path="/TestReactSpring/useTrail" element={<UseTrail />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
